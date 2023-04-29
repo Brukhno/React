6c13de473856c24bb9428a1f6b62c8a8e5ce491b
Brukhno <dimabruhno@icloud.com>
2023-04-29 10:59:40 +0300

    git 	new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/componets/Header.jsx 	new file:   src/componets/NavBar.jsx 	new file:   src/componets/Profile.jsx 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './componets/Header';
+import NavBar from './componets/NavBar';
+import Profile from './componets/Profile';
+
+const App =()=> {
+	return (
+		<div className='app-wrapper'>
+		<Header/>
+		<NavBar/>
+		<Profile/>
+		</div>
+	);
+}
+
+
+export default App;
+
---
+++ src/componets/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Profile =()=>{
+    return<div className='content'>
+	<div>
+		<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwYzBYD8kdjHe9VnspYlCQ9jfwz4KbBZkBNA&usqp=CAU' alt='no img'/>
+		</div>
+		<div>ava</div>
+		<div>posts<div/>
+		<div>new posts</div>
+		<div>
+			<div>post1</div>
+			<div>post2</div>
+		</div>
+		</div>
+	</div>
+}
+    export default Profile
