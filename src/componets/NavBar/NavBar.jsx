58defaed272313c2a3e48c20bc698a6386a9d519
Brukhno <dimabruhno@icloud.com>
2023-05-01 10:56:10 +0300

    activeClassName

--- src/componets/NavBar/NavBar.jsx
+++ src/componets/NavBar/NavBar.jsx
@@ -5,19 +5,19 @@ import s from "./NavBar.module.css"
 const NavBar =()=>{
     return<nav className={s.nav}>
 		<div className={s.item}>
-		<NavLink to="/profile">Profile</NavLink>
+		<NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
 		</div>
 		<div className={s.item}>
-		<NavLink to="/dialogs">Massges</NavLink>
+		<NavLink to="/dialogs" activeClassName={s.active}>Massges</NavLink>
 		</div>
 		<div className={s.item}>
-		<NavLink to="/news">News</NavLink>
+		<NavLink to="/news" activeClassName={s.active}>News</NavLink>
 		</div>
 		<div className={s.item}>
-		<NavLink to="/music">Music</NavLink>
+		<NavLink to="/music" activeClassName={s.active}>Music</NavLink>
 		</div>
 		<div className={s.item}>
-		<NavLink to="/settings" >Settings</NavLink>
+		<NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
 		</div>
 	</nav>
 }
